e509f5a484b0773541e13a56061bf81ffde4915b
Łukasz Susłowicz <susel.invest@gmail.com>
2025-04-04 16:41:02 +0200

    cookie refresh token

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 class UserInfoView(RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated,)
@@ -57,6 +59,29 @@ class LogoutView(APIView):
         response.delete_cookie("refresh_token")
 
         return response
+    
+# odświeżanie tokena - podtrzymanie zalogowania
+# ...do dokończenia na widoku
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get("refresh_token")
+
+        if not refresh_token:
+            return Response({"error":"Refresh token not provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+
+            response = Response({"message":"Acces token refreshed succesfully"}, status=status.HTTP_200_OK)
+            response.set_cookie(key="access_token", value=access_token,
+                                httponly=True, secure=True, samesite="Strict")
+            return response
+        
+        except InvalidToken:
+            return Response({"error":"Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        
 
            
         
