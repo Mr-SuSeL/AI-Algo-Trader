881a76d13d7ec2ac59ae1045ee2464e059849621
Łukasz Susłowicz <susel.invest@gmail.com>
2025-04-04 13:03:11 +0200

    trick with dashboard name how to change it

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import CustomUser
 
+# To zmienia nagłówek
+admin.site.site_header = "AI Algo Trader Dashboard"
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     pass
